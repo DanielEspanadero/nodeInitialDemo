0fa60473be164ffc4f67514926031c90a9ba1b06
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-31 18:04:15 +0100

    N1 E1

---
+++ app/models/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usersPath = '/user';
+
+        // Middlewares
+        this.middlewares();
+
+        // Routes of my app
+        this.routes();
+    }
+
+    middlewares(){
+        // CORS
+        this.app.use(cors());
+
+        // Reading and parsing the body
+        this.app.use(express.json())
+    };
+
+    routes() {
+        this.app.use(this.usersPath, require('../routes/users.routes'));
+    };
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Listening in the port ${this.port}`);
+        })
+    };
+
+
+
+
+};
+
+module.exports = Server;
