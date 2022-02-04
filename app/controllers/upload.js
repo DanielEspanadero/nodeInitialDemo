a2aa02d24f0fe68e2d923c802bc5b277197d238f
Daniel Españadero <developer@daniel-espanadero.com>
2022-02-04 02:25:13 +0100

    Estructura de front-end

--- app/controllers/upload.js
+++ app/controllers/upload.js
@@ -1,9 +1,16 @@
-const uploadPost = (req, res)=>{
+const path = require('path');
+
+const uploadGet = (req, res) => {
+    res.sendFile(path.join(`${__dirname}../../public/html/upload.html`))
+}
+
+const uploadPost = (req, res) => {
     res.json({
         msg: 'Archivo enviado'
     })
 };
 
 module.exports = {
-    uploadPost
+    uploadPost,
+    uploadGet
 };
--- app/routes/upload.js
+++ app/routes/upload.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-const { uploadPost } = require('../controllers/upload');
+const { uploadPost, uploadGet } = require('../controllers/upload');
 const { upload } = require('../middlewares/upload');
 
 
 const router = Router();
 
-
+router.get('/', uploadGet);
 router.post('/', upload, uploadPost);
 
 module.exports = router;
